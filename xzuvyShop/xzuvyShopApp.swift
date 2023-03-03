eeaa9e0ff42b886e78e62f3348795a8ecf2ebe41
Pete <xzuvy27@gmail.com>
2023-03-03 17:51:15 +0300

    Initial Commit

---
+++ xzuvyShop/xzuvyShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  xzuvyShopApp.swift
+//  xzuvyShop
+//
+//  Created by Peter Clarke on 03.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct xzuvyShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
